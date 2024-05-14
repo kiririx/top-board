03476b987a9086565c429fcdf014cfd02d8973c9
Lilithia <kiririx1213@gmail.com>
2024-05-14 23:00:07 +0800

    feat: ckeditor

--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -1,6 +1,16 @@
 import {FC, useState} from "react";
 import {Button, Dropdown, MenuProps, Tabs} from "antd";
 import {PlusOutlined} from '@ant-design/icons';
+import {CKEditor} from "@ckeditor/ckeditor5-react";
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter';
+import Image from '@ckeditor/ckeditor5-image/src/image';
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 
 export const Home: FC = () => {
 
@@ -8,21 +18,40 @@ export const Home: FC = () => {
         {
             key: "def",
             label: "Panel_1",
-            children: <></>
+            children: <div style={{
+                height: "100vh"
+            }}><CKEditor config={{
+
+                plugins: [Essentials, Paragraph, Image, ImageToolbar, ImageCaption, ImageStyle, Base64UploadAdapter],
+                toolbar: {
+                    items: [
+                        'undo', 'redo',
+                        '|', 'heading',
+                        '|', 'bold', 'italic',
+                        '|', 'link', 'insertImage', 'insertTable', 'mediaEmbed', 'blockQuote',
+                        '|', 'bulletedList', 'numberedList', 'outdent', 'indent'
+                    ]
+                },
+                image: {
+                    toolbar: ['imageStyle:full', 'imageStyle:side', '|', 'imageTextAlternative']
+                }
+            }} editor={ClassicEditor} data={"czxczxcz"}  onChange={ ( event ) => {
+                console.log( event );
+            } }></CKEditor></div>
         }
     ]);
     const items: MenuProps['items'] = [
         {
             label: <a>Editor</a>,
-            key: '0',
+            key: 'editor',
         },
         {
             label: <a>Image</a>,
-            key: '1',
+            key: 'image',
         },
         {
             label: <a>PDF</a>,
-            key: '3',
+            key: 'pdf',
         },
     ];
     const addButton = () => {
@@ -31,6 +60,7 @@ export const Home: FC = () => {
         </Dropdown>
     }
     return <>
-        <Tabs tabBarExtraContent={addButton()} items={tabs}/>
+        <Tabs style={{
+        }} tabBarExtraContent={addButton()} items={tabs}/>
     </>
 }
